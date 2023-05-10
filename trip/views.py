feecd4cdeb7a3acbb4cd2bcb07feecd8c8410512
yonasketema <yonask.kitaw@gmail.com>
2023-05-10 13:05:18 +0300

    add trip  serializers

--- trip/views.py
+++ trip/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .models import Trip, Book
+from .serializers import TripSerializer, BookSerializer
 
 
+@api_view()
 def get_trip(request):
-    return HttpResponse('Paradise')
+
+    # query = Book.objects.all()
+    query = Trip.objects.all()
+
+    serializer = TripSerializer(query, many=True)
+
+    return Response(serializer.data)
