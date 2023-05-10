9d0abe3692f7e035e5d53d2adaa4ac371cda2e82
yonasketema <yonask.kitaw@gmail.com>
2023-05-10 13:04:33 +0300

    add trip

--- trip/admin.py
+++ trip/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Trip, Book
+
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price']
+    list_editable = ['price']
+    list_per_page = 12
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['trip', 'booked_at', 'payment_status', ]
--- trip/models.py
+++ trip/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Trip(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Book(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+
+    booked_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    trip = models.ForeignKey(
+        Trip, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return f'{self.trip.title} {self.booked_at} '
