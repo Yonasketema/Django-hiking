29f2c775fb59895527fbd20f85a43fc26d98087b
yonasketema <yonask.kitaw@gmail.com>
2023-05-18 15:56:07 +0300

    config token life time

--- hahuTrip/settings.py
+++ hahuTrip/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,6 +144,7 @@ AUTH_USER_MODEL = 'core.User'
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7)
 }
 
 
