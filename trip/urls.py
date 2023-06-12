196a6d1a3da4403cd5e05a026bb4c10f0d4ffdd0
yonasketema <yonask.kitaw@gmail.com>
2023-06-12 19:57:40 +0300

    add hike

--- trip/urls.py
+++ trip/urls.py
@@ -4,13 +4,18 @@ from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
 router.register('trips', views.TripViewSet)
-# router.register('booking', views.TripBookingViewSet)
+router.register('hikings', views.WeeklyHikeSet)
+
 
 trip_router = routers.NestedDefaultRouter(router, 'trips', lookup='trip')
 trip_router.register('booking', views.TripBookingViewSet,
                      basename='trip-booking')
 
+hike_router = routers.NestedDefaultRouter(router, 'hikings', lookup='hike')
+hike_router.register('booking', views.HikeBookingViewSet,
+                     basename='hike-booking')
+
 
 urlpatterns = [
     path('mytrip', views.mytrip)
-] + router.urls + trip_router.urls
+] + router.urls + trip_router.urls + hike_router.urls
--- trip/views.py
+++ trip/views.py
@@ -3,8 +3,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Trip, TripBooking
-from .serializers import TripSerializer, BookingSerializer
+from .models import Trip, TripBooking, HikingBooking, WeeklyHike
+from .serializers import TripSerializer, TripBookingSerializer, WeeklyHikeSerializer, HikeBookingSerializer
 
 
 class TripViewSet(ModelViewSet):
@@ -12,18 +12,32 @@ class TripViewSet(ModelViewSet):
     serializer_class = TripSerializer
 
 
+class WeeklyHikeSet(ModelViewSet):
+    queryset = WeeklyHike.objects.all()
+    serializer_class = WeeklyHikeSerializer
+
+
 class TripBookingViewSet(ModelViewSet):
     queryset = TripBooking.objects.all()
-    serializer_class = BookingSerializer
+    serializer_class = TripBookingSerializer
     permission_classes = [IsAuthenticated]
 
     def get_serializer_context(self):
         return {'trip_id':  self.kwargs['trip_pk'], 'user_id': self.request.user, 'request': self.request}
 
 
+class HikeBookingViewSet(ModelViewSet):
+    queryset = HikingBooking.objects.all()
+    serializer_class = HikeBookingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_context(self):
+        return {'hike_id':  self.kwargs['hike_pk'], 'user_id': self.request.user, 'request': self.request}
+
+
 @api_view()
 @permission_classes([IsAuthenticated])
 def mytrip(resquest):
     mytrip = TripBooking.objects.filter(user=resquest.user)
-    serializer = BookingSerializer(mytrip, many=True)
+    serializer = TripBookingSerializer(mytrip, many=True)
     return Response(serializer.data)
